4f3cf5ded0779d9abb79d8c2cf85abd65e46e738
Harshal <harshalts@gmail.com>
2021-02-02 00:17:30 +0400

    Added more information about the different characteristics of the character chun li

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,113 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "ID-Card",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('ID Details'),
-        ),
-        body: Row(
-          children: [
+      home: Home(),
+    );
+  }
+}
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: Center(
+              child: Text("ID-Details")
+          ),
+          elevation: 0.0,
+        ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 20, 40, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children : [
+            Container(
+              child: Text(
+                  "NAME",
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              child: Text(
+                  "Chun - Li",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Divider(
+              height: 30.0,
+              color: Colors.grey[600],
+              thickness: 1.0,
+            ),
+
+            SizedBox(height: 6.0),
+            Container(
+              child: Text(
+                "TYPE",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              child: Text(
+                "Fighter",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Divider(
+              height: 30.0,
+              color: Colors.grey[600],
+              thickness: 1.0,
+            ),
+
+            SizedBox(height: 6.0),
+            Container(
+              child: Text(
+                "LEVEL",
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Container(
+              child: Text(
+                "0",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.amber,
+          child: Icon(
+              Icons.add,
+            color: Colors.black,
+          ),
       ),
     );
   }
